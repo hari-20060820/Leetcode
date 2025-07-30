75145eb4e0167006260bd0776a55b10ef09a1a14
Harinesh selvakumar <harineshselvakumar2006@gmail.com>
2025-07-30 21:06:13 +0530

    Time: 23 ms (21.41%) | Memory: 12.5 MB (49.50%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -4,19 +4,43 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        s=str(x)
-        if(x>=0 ):
-            s=s[::-1]
-            if(int(s)<=(2**31)-1):
-                return int(s)
-            else:
-                return 0
-        elif(x<=0 and x>=-(2**31)):
-            s=s[0]+s[1:][::-1]
-            if(int(s)>=-(2**31)):
-                return int(s)
-            else:
-                return 0
-            return int(s)
+        MAX_INT = (2**31) - 1
+        MIN_INT = -(2**31)
+
+        reversed_int = 0
         
-        
+        # Determine the sign and work with the absolute value
+        sign = 1
+        if x < 0:
+            sign = -1
+            x = -x # Work with the positive version for digit reversal
+
+        while x != 0:
+            digit = x % 10  # Get the last digit
+            
+            # Check for overflow/underflow BEFORE adding the new digit
+            # For positive numbers:
+            # If reversed_int > MAX_INT // 10, then reversed_int * 10 will definitely overflow.
+            # If reversed_int == MAX_INT // 10, then check if adding 'digit' will overflow.
+            # (MAX_INT % 10) is 7, so if digit > 7, it overflows.
+            if sign == 1 and (reversed_int > MAX_INT // 10 or \
+                              (reversed_int == MAX_INT // 10 and digit > 7)):
+                return 0
+            
+            # For negative numbers (when dealing with the positive absolute value):
+            # The actual MIN_INT is -2147483648. Its absolute value is 2147483648.
+            # MAX_INT // 10 is 214748364.
+            # If reversed_int > MIN_INT_ABS // 10 (which is 214748364), it will underflow.
+            # If reversed_int == MIN_INT_ABS // 10, then check if adding 'digit' will underflow.
+            # (MIN_INT_ABS % 10) is 8, so if digit > 8, it underflows.
+            # This check is a bit trickier because we're building a positive `reversed_int`
+            # and then applying the negative sign at the end. We need to check against
+            # the absolute value of MIN_INT.
+            if sign == -1 and (reversed_int > MAX_INT // 10 or \
+                              (reversed_int == MAX_INT // 10 and digit > 8)): # 8 because -2147483648 ends in 8
+                return 0
+
+            reversed_int = reversed_int * 10 + digit # Build the reversed number
+            x //= 10 # Remove the last digit from the original number
+
+        return reversed_int * sign

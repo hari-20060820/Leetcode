af8771fe3dae58d96556b61e3bc465147d3d5c75
Harinesh selvakumar <harineshselvakumar2006@gmail.com>
2025-09-30 21:56:37 +0530

    Time: 75 ms (54.81%) | Memory: 98.6 MB (54.04%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -6,7 +6,7 @@ public:
         unordered_map<int,int> w;
         for( int i=0;i<n;i++)
         {
-            if(w.find(nums[i])!= w.end() && abs(i-w[nums[i]])<=k) return true;
+            if(w.find(nums[i])!= w.end() && (i-w[nums[i]])<=k) return true;
 
             w[nums[i]]=i;
            

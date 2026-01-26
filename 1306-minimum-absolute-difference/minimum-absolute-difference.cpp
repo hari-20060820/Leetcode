6213083d3985f785febc8d3c8a5ed1bf5de45a1a
Harinesh selvakumar <harineshselvakumar2006@gmail.com>
2026-01-26 22:46:59 +0530

    Time: 15 ms (76.77%) | Memory: 36.7 MB (64.03%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -1,31 +1,20 @@
-#include <cmath>
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
         vector<vector<int>> res;
+        int sub = abs(arr[0] - arr[1]);
         sort(arr.begin(), arr.end());
-        int l = 0;
-        int r = l + 1;
-        int sub = abs(arr[r] - arr[l]);
-        l++;
-        r++;
-        while (r < arr.size()) {
-            int sub1 = abs(arr[r] - arr[l]);
-            if (sub1< sub) {
-                sub=sub1;
+
+        for (int i = 0; i < arr.size()-1; i++) {
+            int sub1 = abs(arr[i] - arr[i + 1]);
+            if (sub1 < sub) {
+                sub = sub1;
+                res.clear();
+                res.push_back(vector<int>{arr[i], arr[i + 1]});
+            } else if (sub == sub1) {
+                res.push_back(vector<int>{arr[i], arr[i + 1]});
             }
-            l++;
-            r++;
-        }
-        l = 0;
-        r = l + 1;
-        while (r < arr.size()) {
-            int sub1 = abs(arr[r] - arr[l]);
-            if (sub1== sub) {
-                    res.push_back(vector<int>{arr[l],arr[r]});
-            }
-            l++;
-            r++;
+            
         }
         return res;
     }

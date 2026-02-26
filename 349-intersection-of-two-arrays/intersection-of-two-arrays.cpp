d3e6c99f7f283decd29b9abcd7c34f9d4f8f3590
Harinesh selvakumar <harineshselvakumar2006@gmail.com>
2026-02-26 22:50:47 +0530

    Time: 2 ms (42.22%) | Memory: 14.6 MB (13.58%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -4,15 +4,11 @@ public:
         unordered_set<int> a(nums1.begin(),nums1.end());
         unordered_set<int> b(nums2.begin(),nums2.end());
         vector<int> res;
-        if(a.size()>b.size())
-        {
-            swap(a,b);
-        }
         for(int x :a){
             if(b.find(x)!=b.end())
             {
                 res.push_back(x);
-                b.erase(x);
+                
             }
             
         }

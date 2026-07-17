cd99b0d2e62def289eec00b04d9e87babbffb82d
Harinesh selvakumar <harineshselvakumar2006@gmail.com>
2026-07-17 21:45:08 +0530

    Time: 3191 ms (5.04%) | Memory: 18.2 MB (25.59%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -9,18 +9,26 @@ class Solution(object):
         :type lists: List[Optional[ListNode]]
         :rtype: Optional[ListNode]
         """
-        res=[]
-        size=len(lists)
-        for i in range(size):
-            node =lists[i]  
-            while node !=None:
-                res.append(node.val)
-                node=node.next
-        res.sort(reverse=True)
-        node=None
-        head=None
-        for i in res:
-            node=ListNode(i)
-            node.next=head
-            head=node
-        return head
+        def mergetwolist(list1,list2):
+            dummy=ListNode(0)
+            tail=dummy
+            while list1 and list2:
+                if list1.val>list2.val:
+                    tail.next=list2
+                    list2=list2.next
+                else:
+                    tail.next=list1
+                    list1=list1.next
+                tail=tail.next
+            if list1:
+                tail.next=list1
+            if list2:
+                tail.next=list2
+            return dummy.next
+        n=len(lists)
+        if n==0:
+            return
+        ans=lists[0]
+        for i in range(1,n):
+            ans=mergetwolist(ans,lists[i])
+        return ans
